36d9fdd1164104afb94dc07873f92b6bc4354d78
ychiwaye <ychiwaye@e4.co.za>
2024-06-03 08:38:50 +0200

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //string file = "C:\\my documents\\homedirectory\\files";
+            string file = @"C:\my documents\homedirectory\files";
+            Console.WriteLine(file);    
+        }
+    }   
+}
